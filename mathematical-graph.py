2055d749b4fd17d40000f030d7d7fb37790509a5
venusaur20 <venusaur20@gmail.com>
2024-05-29 22:21:39 -0500

    working on it

--- mathematical-graph.py
+++ mathematical-graph.py
@@ -1,12 +1,40 @@
 import matplotlib.pyplot as plt, numpy as np, math
 
 
-x_start = 1 #int(input("Start of x-axis: "))
-x_finish = 100 #int(input("End of x-axis: "))
-accuracy = 0.05 #int(input("Update Interval: "))
-x_axis = np.arange(x_start, x_finish, accuracy)
+x_start = -10 #int(input("Start of x-axis: "))
+x_finish = 10 #int(input("End of x-axis: "))
+accuracy = 0.1 #int(input("Update Interval: "))
+x = np.arange(x_start, x_finish, accuracy)
 
-y_axis = 10**(x_axis)
+function = ()
 
-plt.plot(x_axis, y_axis, 'r--')
+while True:
+    opperation = input("Opperation: ")
+    if opperation == ("e"):
+        break
+    number = input("Number: ")
+
+#Check if the "number" is a variable or number
+    if number == ("x"):
+        number = (x)
+        print(number)
+    else:
+        number = np.arange()
+
+#Opperations
+    if opperation == "+":
+        function = (function + number)
+    elif opperation == "-":
+        function = (function - number)
+    elif opperation == "**":
+        function = (function**number)
+    elif opperation == "/":
+        function = (function/number)
+    elif opperation == "//":
+        function = (function**(1/number))
+
+    print(opperation)
+    print(number)
+
+plt.plot(x, function, 'r--')
 plt.show()
