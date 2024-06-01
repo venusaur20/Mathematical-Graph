2cf1a38e44247d4bf1fe222487e36a9a935368ae
venusaur20 <venusaur20@gmail.com>
2024-05-31 22:51:51 -0500

    reworked the entire back end, added the ability for users to enter any function they want into the calculator at a very basic level

--- mathematical-graph.py
+++ mathematical-graph.py
@@ -1,40 +1,44 @@
 import matplotlib.pyplot as plt, numpy as np, math
 
 
-x_start = -10 #int(input("Start of x-axis: "))
-x_finish = 10 #int(input("End of x-axis: "))
-accuracy = 0.1 #int(input("Update Interval: "))
-x = np.arange(x_start, x_finish, accuracy)
+x_values = np.linspace(-100, 100, 2000)
 
-function = ()
+def plot():
+    plt.figure(figsize=(12, 8))
+    plt.plot(x_values, y_values, '-', color='green')
+    plt.show()
+    plt.close()
 
-while True:
-    opperation = input("Opperation: ")
-    if opperation == ("e"):
-        break
-    number = input("Number: ")
 
-#Check if the "number" is a variable or number
-    if number == ("x"):
-        number = (x)
-        print(number)
-    else:
-        number = np.arange()
+def function_generator():
+    function = (np.linspace(-100, 100, 2000) * 0)
+    while True:
+        opperation = input("Opperation: ")
+        if opperation == ("e"):
+            break
+        
+        number = input("Number: ")
+        if number == "x":
+            number = x_values
+        else:
+            number = int(number)
 
 #Opperations
-    if opperation == "+":
-        function = (function + number)
-    elif opperation == "-":
-        function = (function - number)
-    elif opperation == "**":
-        function = (function**number)
-    elif opperation == "/":
-        function = (function/number)
-    elif opperation == "//":
-        function = (function**(1/number))
+        if opperation == "+":
+            function = (function + number)
+        elif opperation == "-":
+            function = (function - number)
+        elif opperation == "*":
+            function = (function*number)
+        elif opperation == "**":
+            function = (function**number)
+        elif opperation == "/":
+            function = (function/number)
+        elif opperation == "//":
+            function = (function**(1/number))
 
-    print(opperation)
-    print(number)
+    return(function)
+    
 
-plt.plot(x, function, 'r--')
-plt.show()
+y_values = function_generator()
+plot()
